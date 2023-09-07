232584ea60609806edec73fa4b53fa1751ccc3ff
vmantri83 <vmantri83@gmail.com>
2023-09-07 13:02:32 +0530

    added code

---
+++ script.js
@@ -0,0 +1,30 @@
+let winMsg = "Victory";
+let loseMsg = "Crushing Defeat";
+let tieMsg = "Tie";
+let moveList = ["Rock", "Paper", "Scissors"];
+/* Computes result of the game. returns victory message if move 1 wins.*/
+
+function calcResult(move1, move2) {}
+
+/*
+ * @return {Number}   random number between 0 and 2
+ */
+
+function randomMove() {
+  return Math.floor(Math.random() * 3);
+}
+
+/*
+ * Displays start state of game
+ */
+
+function startGame() {}
+
+/*
+ * Displays end state of game
+ * @param {Event} event event containing information of users input.
+ */
+
+function endGame(event) {}
+
+startGame();
